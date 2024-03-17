5ff8f75e467e383e7897dbb2c08e6c980977dad0
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2024-03-17 10:10:13 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+
+const userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.131 Safari/537.36'
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 600,
+        height: 600,
+    })
+    
+    win.loadURL('https://web.whatsapp.com', { userAgent })
+    // A webpage to be loaded
+
+    return win
+
+    // win.loadFile("./src/index.html")
+    //An HTML file to be loaded
+}
+
+app.whenReady().then(()=> {
+    createWindow()
+})
